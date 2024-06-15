01839cec8edb477edb7e0dc7ecb6e9ac76ba47be
kwarchal <kamilwarchal295@gmail.com>
2024-06-15 19:20:42 +0200

    druga czesc programu

--- src/liczbyPierwszeWzakresach/Main.java
+++ src/liczbyPierwszeWzakresach/Main.java
@@ -28,14 +28,23 @@ class liczbyPierwsze
     }
 
     //sprawdzamy czy dana liczba jest liczba pierwsza
+    private boolean czyPierwsza(int liczba)
+    {
+        return liczba > 1 && IntStream
+                .range(2, liczba)
+                .noneMatch(i -> liczba % i == 0);
+    }
 
 }
-
-
 
 public class Main {
     public static void main(String[] args)
     {
+        liczbyPierwsze p = new liczbyPierwsze();
+
+        System.out.println("Program znajduje liczby pierwsze w okreslonych zakresach");
+        System.out.println("Liczby od 2 do 100 to: " + p.pierwszaAZdo(100) +".");
+        System.out.println("Liczby pierwsze od 15 do 60 to: " + p.pierwszaZakres(15, 60) + ".");
 
     }
 }
